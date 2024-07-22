773954f2ece4a3121bef99e6e73ecab9049ca71f
Rodrigo <rodrigoemdiaz@hotmail.com>
2024-07-22 17:33:43 -0500

    Creación de block theme

---
+++ themes/fictional-block-theme/build/genericbutton.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '75a320d31fe5121d2262');
---
+++ themes/fictional-block-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e32e98204707f0bcc579');
