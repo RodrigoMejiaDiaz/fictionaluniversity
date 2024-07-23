72f5d01f70a24935e638f85cf6c96e3f07806d0d
Rodrigo <rodrigoemdiaz@hotmail.com>
2024-07-23 17:16:12 -0500

    193. Our PHP Render Approach

--- themes/fictional-block-theme/build/banner.asset.php
+++ themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'e907859bef4d363e2562');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '6f313fdfdc597bd2e710');
