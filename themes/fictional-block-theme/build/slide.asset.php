c1e587e65a125e2f20bb30f6ec11764432d52a01
Rodrigo <rodrigoemdiaz@hotmail.com>
2024-07-24 10:31:34 -0500

    202. Slideshow Block 1

---
+++ themes/fictional-block-theme/build/slide.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '8c7952f555e23116452c');
---
+++ themes/fictional-block-theme/build/slideshow.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '85fbdb1e62cdd7b971b0');
