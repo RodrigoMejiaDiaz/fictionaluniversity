435cc5bcad1b35369ea1a294662dc9b6ec974369
Rodrigo <rodrigoemdiaz@hotmail.com>
2024-07-23 19:17:39 -0500

    198. Quick Details: Banner Block

--- themes/fictional-block-theme/build/banner.asset.php
+++ themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'f7b74002765aa99fb350');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '108b024e2b4f2b00f309');
